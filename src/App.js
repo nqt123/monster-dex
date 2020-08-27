0c7d07859ef8e11d06d7bec63c695aa418de739b
Quy Thang <quythang1997@gmail.com>
2020-08-27 22:11:09 +0700

    Init Monster Dex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monster: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monster: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type='search' placeholder='Search Monster' onChange={e => this.setState({ searchField: e.target.value })
+        } />
+        <CardList monster={this.state.monster} />
+      </div>
+    );
+  }
 }
 
 export default App;
