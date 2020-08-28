213bae019d0623eb2e08f82f219d061f884dd70d
Quy Thang <quythang1997@gmail.com>
2020-08-28 22:57:30 +0700

    Update css, index.html

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1>Monster Dex</h1>
         <SearchBox
           placeholder="Monster Finder"
           handleChange={this.handleChange}
