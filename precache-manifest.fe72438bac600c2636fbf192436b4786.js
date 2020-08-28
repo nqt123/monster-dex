08101d8348e1c01913f41d1955e2850894690168
Quy Thang <quythang1997@gmail.com>
2020-08-28 23:00:22 +0700

    Updates

---
+++ precache-manifest.fe72438bac600c2636fbf192436b4786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134a650f80c884fac3a2ad2d69c5f562",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "ba234d635b70784b2256",
+    "url": "/monster-dex/static/css/main.d8fe807a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-dex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-dex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba234d635b70784b2256",
+    "url": "/monster-dex/static/js/main.edab2756.chunk.js"
+  },
+  {
+    "revision": "c86287012d6d508cfa09",
+    "url": "/monster-dex/static/js/runtime-main.3266a2fd.js"
+  }
+]);
